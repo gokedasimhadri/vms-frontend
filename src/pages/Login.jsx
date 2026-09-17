13f99fe6dcf0c58f99e64166c94c19ae23144517
Suresh_Pantham <nagagangasureshp@adityauniversity.in>
2026-09-17 10:32:26 +0530

    signup removed.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../services/api';
 
 const Login = () => {
@@ -79,10 +79,6 @@ const Login = () => {
             {isLoading ? 'Signing In...' : 'Sign In'}
           </button>
         </form>
-        
-        <div className="auth-footer">
-          Don't have an account? <Link to="/register">Create one now</Link>
-        </div>
       </div>
     </div>
   );
